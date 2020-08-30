d4379e8674c82759261450335f7fd41e8401f617
Andrey Semenov <andss82@gmail.com>
2020-08-30 05:57:26 +0300

    task 1 done

---
+++ cmd/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	server "github.com/i-hit/go-lesson3.2.git/pkg"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+
+	if err := execute(); err != nil {
+		os.Exit(1)
+	}
+
+}
+
+func execute() (err error) {
+	listener, err := net.Listen("tcp", "0.0.0.0:9999")
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	defer func() {
+		if cerr := listener.Close(); cerr != nil {
+			log.Println(err)
+			return
+		}
+	}()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		server.Handle(conn)
+	}
+}
